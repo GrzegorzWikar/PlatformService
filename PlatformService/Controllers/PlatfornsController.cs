e05f0d63d75893e8f1995e88687e21705c310dc7
GrzegorzWikar <grzegorzwikar938@gmail.com>
2023-07-27 15:14:44 +0200

    Docker

--- PlatformService/Controllers/PlatfornsController.cs
+++ PlatformService/Controllers/PlatfornsController.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
 using PlatformService.Dtos;
+using PlatformService.Models;
 
 namespace PlatformService.Controllers
 {
@@ -26,6 +27,26 @@ namespace PlatformService.Controllers
             var platformItems = _repository.GetAllPlatforms();
 
             return Ok(_mappre.Map<IEnumerable<PlatformReadDtos>>(platformItems));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDtos> GetPlatformById(int id)
+        {
+            var platformItem = _repository.GetPlatformById(id);
+            if (platformItem != null) return Ok(_mappre.Map<PlatformReadDtos>(platformItem));
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDtos> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            var platformModel = _mappre.Map<Platform>(platformCreateDto);
+            _repository.CreatePlatform(platformModel);
+            _repository.SaveChanges();
+
+            var platformReadDto = _mappre.Map<PlatformReadDtos>(platformModel);
+
+            return CreatedAtRoute(nameof(GetPlatformById), new { IdentityServiceCollectionExtensions = platformReadDto.Id }, platformReadDto);
         }
     }
 }
